9895ec85b1de7699e2e5455fdcc39744c649a4c2
intr0vrt <pankevychy@uni.coventry.ac.uk>
2026-03-20 13:24:31 +0200

    add new structure

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -11,9 +11,9 @@ import asyncio
 import sys
 import os
 
-from fraud_detection import check_fraud
-from transaction_verification import verify_transaction
-from recommendation import get_recommendations
+# from fraud_detection import check_fraud
+# from transaction_verification import verify_transaction
+# from recommendation import get_recommendations
 
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
 utils_path = os.path.abspath(os.path.join(FILE, '../../../utils/'))
@@ -21,6 +21,16 @@ sys.path.insert(0, utils_path)
 from log_utils.logger import setup_logger
 logger = setup_logger("Orchestrator")
 logger.addHandler(default_handler)
+
+# import pb.transaction_verification.transaction_verification_pb2 as transaction_verification
+# import pb.transaction_verification.transaction_verification_pb2_grpc as transaction_verification_grpc
+# import pb.order_details.order_details_pb2 as order_details
+
+import pb.transaction_verification_pb2 as transaction_verification
+import pb.transaction_verification_pb2_grpc as transaction_verification_grpc
+import pb.order_details_pb2 as order_details
+
+import grpc
 
 # Create a simple Flask app.
 app = Flask(__name__)
@@ -46,6 +56,48 @@ def transform_results(results: list[dict]):
         for result in results
     }
 
+
+async def init_transaction(request_data, order_id, connection_string, stub_class):
+    async with grpc.aio.insecure_channel(connection_string) as channel:
+        stub = stub_class(channel)
+        user_info = order_details.User(**request_data["user"])
+        credit_card_info = order_details.CreditCard(
+            number=request_data["creditCard"]["number"],
+            expiration_date=request_data["creditCard"]["expirationDate"],
+            cvv=request_data["creditCard"]["cvv"]
+        )
+        items = [order_details.OrderItem(**item) for item in request_data["items"]]
+        billing_address_info = order_details.BillingAddress(**request_data["billingAddress"])
+        fraud_request = order_details.InputOrderDetails(
+            order_id=order_id,
+            user=user_info,
+            credit_card=credit_card_info,
+            user_comment=request_data["userComment"] or "",
+            items=items,
+            billing_address=billing_address_info,
+            shipping_method=request_data["shippingMethod"],
+            gift_wrapping=request_data["giftWrapping"],
+            terms_accepted=request_data["termsAccepted"]
+        )
+        response = await stub.InitTransaction(fraud_request)
+    
+    return response
+
+
+async def call_action(order_id, connection_string, stub_class, method_name, vector_clock=[0,0,0]):
+    async with grpc.aio.insecure_channel(connection_string) as channel:
+        stub = stub_class(channel)
+        fraud_request = order_details.OperationalMessage(
+            order_id=order_id,
+            vector_clock=vector_clock,
+        )
+        method = getattr(stub, method_name)
+        response = await method(fraud_request)
+
+    return response
+
+
+
 @app.route('/checkout', methods=['POST'])
 async def checkout():
     """
@@ -59,27 +111,46 @@ async def checkout():
     order_id = '12345'
     suggested_books = []
 
-    parallel_results = await asyncio.gather(
-        check_fraud(request_data),
-        verify_transaction(request_data),
+    # parallel_results = await asyncio.gather(
+    #     check_fraud(request_data),
+    #     verify_transaction(request_data),
+    # )
+    # results = transform_results(parallel_results)
+
+    statuses = await asyncio.gather(
+        init_transaction(request_data, order_id, "transaction_verification:50052", transaction_verification_grpc.TransactionVerificationServiceStub)
     )
-    results = transform_results(parallel_results)
+
 
     order_response = {
         'orderId': order_id,
         'suggestedBooks': suggested_books
     }
 
-    if results["fraud_detection"]["is_fraud"]:
+    # Call the action with the appropriate parameters.
+    results = await asyncio.gather(
+        call_action(order_id, "transaction_verification:50052", transaction_verification_grpc.TransactionVerificationServiceStub, "VerifyItems", vector_clock=[0,0,0])
+    )
+    result = results[0]
+
+    if not result.status.success:
         order_response["status"] = "Order Denied"
-        order_response["errorMessage"] = results['fraud_detection']['error_message']
-    elif not results["transaction_verification"]["transaction_valid"]:
-        order_response["status"] = "Order Denied"
-        order_response["errorMessage"] = results['transaction_verification']['error_message']
+        order_response["errorMessage"] = result.status.error_message
     else:
         order_response["status"] = "Order Approved"
-        recommendation_result = await get_recommendations(request_data)
-        order_response["suggestedBooks"] = recommendation_result["data"]["suggested_books"]
+
+    # if results["fraud_detection"]["is_fraud"]:
+    #     order_response["status"] = "Order Denied"
+    #     order_response["errorMessage"] = results['fraud_detection']['error_message']
+    # elif not results["transaction_verification"]["transaction_valid"]:
+    #     order_response["status"] = "Order Denied"
+    #     order_response["errorMessage"] = results['transaction_verification']['error_message']
+    # else:
+    #     order_response["status"] = "Order Approved"
+    #     recommendation_result = await get_recommendations(request_data)
+    #     order_response["suggestedBooks"] = recommendation_result["data"]["suggested_books"]
+
+
     
     return order_response
 
---
+++ utils/service_wrappers/base_service_wrapper.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+def init_grpc_pathes():
+    FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
+    utils_path = os.path.abspath(os.path.join(FILE, '../../'))
+    print(f"utils_path: {utils_path}")
+    sys.path.insert(0, utils_path)
+    
+    FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
+    order_path = os.path.abspath(os.path.join(FILE, '../../utils/pb/order_details'))
+    print(f"order_path: {order_path}")
+    sys.path.insert(0, order_path)
+
+
+init_grpc_pathes()
+
+
+# import pb.order_details.order_details_pb2 as order_details
+import pb.order_details_pb2 as order_details
+import grpc
+
+
+class BaseServiceWrapper:
+    def __init__(self, service_id, n_services):
+        self.service_id = service_id
+        self.n_services = n_services
+        self.vector_clock = [0] * n_services
+
+        self.order_details = dict()
+
+    def InitTransaction(self, request, context):
+        self.order_details[request.order_id] = request
+        return order_details.StatusMessage(
+            success = True,
+            order_id = request.order_id
+        )
+
+    def ClearTransaction(self, request, context):
+        if request.order_id in self.order_details:
+            del self.order_details[request.order_id]
+        return order_details.StatusMessage(
+            success = True,
+            order_id = request.order_id
+        )
+
+    def _update_vector_clock(self, incoming_vector_clock):
+        for i in range(self.n_services):
+            self.vector_clock[i] = max(self.vector_clock[i], incoming_vector_clock[i])
+        self.vector_clock[self.service_id] += 1
+
+    def _send_request_to_service(self, stub_class, connection_string, method_name, message):
+        # message.vector_clock = self.vector_clock
+        with grpc.insecure_channel(connection_string) as channel:
+            stub = stub_class(channel)
+            method = getattr(stub, method_name)
+            response = method(message)
+        return response
