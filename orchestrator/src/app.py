6a49e2c225d2474937691c044b173d7ac469aa2f
xxwvlyx <alwaysxjun@gmail.com>
2026-03-20 12:57:21 +0200

    try to make proces parallel

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -1,17 +1,17 @@
 import sys
 import os
-import random
+import uuid
+import threading
 
-# This set of lines are needed to import the gRPC stubs.
-# The path of the stubs is relative to the current file, or absolute inside the container.
-# Change these lines only if strictly needed.
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
 fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/fraud_detection'))
 transaction_verification_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
 suggestions_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/suggestions'))
+
 sys.path.insert(0, fraud_detection_grpc_path)
 import fraud_detection_pb2 as fraud_detection
 import fraud_detection_pb2_grpc as fraud_detection_grpc
+
 sys.path.insert(0, transaction_verification_grpc_path)
 import transaction_verification_pb2 as transaction_verification
 import transaction_verification_pb2_grpc as transaction_verification_grpc
@@ -21,113 +21,136 @@ import suggestions_pb2 as suggestions
 import suggestions_pb2_grpc as suggestions_grpc
 
 import grpc
-
-
 import logging
 
-# Configure logging to file and console
 logging.basicConfig(
-    filename="/logs/orchestrator_logs.txt",
-    filemode="a",
-    format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
     level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
 )
 
 logger = logging.getLogger(__name__)
 
-def detect_fraud(card_nr, order_ammount):
-    # Establish a connection with the fraud-detection gRPC service.
-    with grpc.insecure_channel('fraud_detection:50051') as channel:
-        # Create a stub object.
-        stub = fraud_detection_grpc.FraudDetectionServiceStub(channel)
-        # Call the service through the stub object.
-        response = stub.checkFraud(fraud_detection.FraudRequest(card_nr=card_nr, order_ammount=order_ammount))
-        if response.is_fraud:
-            logger.warning(f"Fraud detected for card {card_nr} with amount {order_ammount}")
-    return response.is_fraud
-
-def verify_transaction(card_nr, order_id, money):
-    with grpc.insecure_channel('transaction_verification:50052') as channel:
-        # Create a stub object.
-        stub = transaction_verification_grpc.transactionServiceStub(channel)
-        # Call the service through the stub object.
-        response = stub.verifyTransaction(transaction_verification.PayRequest(card_nr=str(card_nr), order_id=order_id, money=money))
-        logger.info(f"Transaction with {card_nr} {order_id} {money} result {response.verified}")
-        if response.order_id != order_id: return False
-    return response.verified
-
-def get_suggested_books(ordered_books):
-    with grpc.insecure_channel("suggestions:50053") as channel:
-        stub = suggestions_grpc.SuggestionsServiceStub(channel)
-
-        response = stub.suggest(suggestions.SuggestRequest(ordered_books=ordered_books))
-    return response.suggested_books
-
-# Import Flask.
-# Flask is a web framework for Python.
-# It allows you to build a web application quickly.
-# For more information, see https://flask.palletsprojects.com/en/latest/
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 
-# Create a simple Flask app.
 app = Flask(__name__)
-# Enable CORS for the app.
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# Define a GET endpoint.
+
+def detect_fraud(card_nr, amount):
+    with grpc.insecure_channel('fraud_detection:50051') as channel:
+        stub = fraud_detection_grpc.FraudDetectionServiceStub(channel)
+        response = stub.checkFraud(
+            fraud_detection.FraudRequest(
+                card_nr=card_nr,
+                order_ammount=amount
+            )
+        )
+        return response.is_fraud
+
+
+def verify_transaction(card_nr, order_id, amount):
+    with grpc.insecure_channel('transaction_verification:50052') as channel:
+        stub = transaction_verification_grpc.transactionServiceStub(channel)
+        response = stub.verifyTransaction(
+            transaction_verification.PayRequest(
+                card_nr=str(card_nr),
+                order_id=order_id,
+                money=amount
+            )
+        )
+        return response.verified
+
+
+def get_suggestions(books):
+    with grpc.insecure_channel("suggestions:50053") as channel:
+        stub = suggestions_grpc.SuggestionsServiceStub(channel)
+        response = stub.suggest(
+            suggestions.SuggestRequest(ordered_books=books)
+        )
+        return response.suggested_books
+
+
 @app.route('/', methods=['GET'])
 def index():
-    """
-    Responds with 'Hello, [name]' when a GET request is made to '/' endpoint.
-    """
-    # Return the response.
     return "hello orchestrator"
+
 
 @app.route('/checkout', methods=['POST'])
 def checkout():
-    """
-    Responds with a JSON object containing the order ID, status, and suggested books.
-    """
-    # Get request object data to json
-    request_data = json.loads(request.data)
-    # Print request object data
+    data = request.get_json()
 
-    quantity = sum([item["quantity"] for item in request_data["items"]])
+    order_id = str(uuid.uuid4())
+    logger.info(f"OrderID: {order_id}")
 
-    is_fraud = detect_fraud(request_data["creditCard"]["number"], quantity)
-    suggested_books = get_suggested_books([i["name"] for i in request_data["items"]])
-    logger.info(f"Got suggested books.")
+    items = data["items"]
+    card = data["creditCard"]["number"]
 
-    # Convert the gRPC response to a dictionary
-    suggested_books_dicts = []
-    for book in suggested_books:
-        suggested_books_dicts.append({
-            'bookId': book.bookId,
-            'title': book.title,
-            'author': book.author
-        })
+    quantity = sum([i["quantity"] for i in items])
+    book_names = [i["name"] for i in items]
 
-    # Generate order_id
-    order_id = random.randint(0, 2**63 - 1)
-
-    verified = verify_transaction(request_data["creditCard"]["number"], order_id, quantity)
-    if not verified:
-        is_fraud = True
-
-    # Dummy response following the provided YAML specification for the bookstore
-    order_status_response = {
-        'orderId': str(order_id),
-        'status': ('Order Rejected' if is_fraud else 'Order Approved'),
-        'suggestedBooks': suggested_books_dicts if not is_fraud else [],
+    result = {
+        "fail": False,
+        "fraud": False,
+        "verified": False,
+        "suggestions": []
     }
 
-    return jsonify(order_status_response)
+    #verify items
+    def event_a():
+        if len(items) == 0:
+            result["fail"] = True
+            logger.warning("Event A failed: empty items")
+
+
+    #verify user data
+    def event_b():
+        if not card:
+            result["fail"] = True
+            logger.warning("Event B failed: no card")
+
+    # RUN A || B
+    t1 = threading.Thread(target=event_a)
+    t2 = threading.Thread(target=event_b)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    if result["fail"]:
+        return jsonify({"status": "Order Rejected", "orderId": order_id})
+
+    #transaction verification
+    result["verified"] = verify_transaction(card, int(uuid.uuid4().int & (1<<32)-1), quantity)
+
+    if not result["verified"]:
+        return jsonify({"status": "Order Rejected", "orderId": order_id})
+
+    #fraud check
+    result["fraud"] = detect_fraud(card, quantity)
+
+    if result["fraud"]:
+        return jsonify({"status": "Order Rejected", "orderId": order_id})
+
+    #suggestions
+    books = get_suggestions(book_names)
+
+    suggestions_list = []
+    for b in books:
+        suggestions_list.append({
+            "bookId": b.bookId,
+            "title": b.title,
+            "author": b.author
+        })
+
+    return jsonify({
+        "orderId": order_id,
+        "status": "Order Approved",
+        "suggestedBooks": suggestions_list
+    })
 
 
 if __name__ == '__main__':
-    # Run the app in debug mode to enable hot reloading.
-    # This is useful for development.
-    # The default port is 5000.
     app.run(host='0.0.0.0')
